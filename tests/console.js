a62aa1cd5710f9b7f471aafe9a17aa559f584d09
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-03 18:48:06 -0500

    -

--- tests/console.js
+++ tests/console.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env -S deno run --allow-all
-const { Console, red } = await import("../main/console.js")
+const { Console, clearStylesFrom, black, white, red, green, blue, yellow, cyan, magenta, lightBlack, lightWhite, lightRed, lightGreen, lightBlue, lightYellow, lightMagenta, lightCyan, blackBackground, whiteBackground, redBackground, greenBackground, blueBackground, yellowBackground, magentaBackground, cyanBackground, lightBlackBackground, lightRedBackground, lightGreenBackground, lightYellowBackground, lightBlueBackground, lightMagentaBackground, lightCyanBackground, lightWhiteBackground, bold, reset, dim, italic, underline, inverse, hidden, strikethrough, visible, gray, grey, lightGray, lightGrey, grayBackground, greyBackground, lightGrayBackground, lightGreyBackground, } = await import("../main/console.js")
 
 Console.env.THING = "true"
 
