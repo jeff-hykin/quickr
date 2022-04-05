b0ed65923021c146c16fc78987184ddc0589842b
Jeff Hykin <jeff.hykin@gmail.com>
2022-04-05 12:40:51 -0500

    -

--- tests/misc.js
+++
@@ -1,8 +0,0 @@
-// import Babel from "../dependencies/babel@v7.13.8.js"
-
-// console.log(Babel.availablePlugins)
-// // export const bundle = ({inputPath, outputPath, esVersion, }) => {
-    
-// // }
-
-export const test = 10
