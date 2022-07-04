f034c41db2eb525175b7c3fb047463b2511aff2a
Jeff Hykin <jeff.hykin@gmail.com>
2022-07-04 09:38:21 -0500

    decide on stdout/err approach

--- main/smash.js
+++ main/smash.js
@@ -1,5 +1,19 @@
 import { Event, trigger, everyTime, once } from "https://deno.land/x/good@0.5.14/events.js"
 
+const notGiven = Symbol()
+const taskSymbol = Symbol("task")
+
+// 
+// examples
+// 
+    // await run`echo hi`.stdout({overwrite: "./my_file.txt"})
+    // await run`cat .zshrc`.with({ cwd: OperatingSystem.home }).pipeTo(
+    //     run`grep ${"thing"}`
+    // ).and(
+    //     run`echo great your .zshrc has a thing`
+    // )
+
+
 // 
 // clean/core logic
 // 
@@ -14,13 +28,148 @@ async function simpleRun({ command, stdin, stdout, stderr, out, cwd, env, intera
 
 // 
 // 
+// detailed logic
+// 
+// 
+function standardizeArguments({ command, stdin, stdout, stderr, out, cwd, env, interactive }) {
+    // 
+    // stdin
+    // 
+    if (stdin == notGiven) {
+        stdin = {
+            fromUser: true,
+            overwrite: [],
+            appendTo: [],
+        }
+    }
+
+    // 
+    // out
+    // 
+    // if its not a file object, ignore the overwrite vs appendTo
+
+
+}
+
+
+
+const methods = (thisTask, outputPromise) => ({
+    [taskSymbol]: thisTask,
+    interactive: ({ onOut, onStdout, onStderr, onFinsh, stdin, stdout, out, cwd, env, timeout })=>{
+        const args = { stdin, stdout, out, cwd, env, timeout, interactive: {onOut, onStdout, onStderr, onFinsh}, }
+        // merge arguments with any existing ones
+        assignArgs(thisTask, args)
+        return outputPromise
+    },
+    with: ({ stdin, stdout, out, cwd, env, timeout })=>{
+        const args = { stdin, stdout, out, cwd, env, timeout }
+        // merge arguments with any existing ones
+        assignArgs(thisTask, args)
+        return outputPromise
+    },
+    stdout: ({ overwrite, appendTo, })=>{
+        if (!(thisTask.stdout instanceof Object)) {
+            thisTask.stdout = {
+                overwrite: [],
+                appendTo: [],
+            }
+        }
+        
+        // merge arguments with any existing ones
+        thisTask.stdout = {
+            overwrite: thisTask.stdout.overwrite.cat(makeArray(overwrite)),
+            appendTo: thisTask.stdout.appendTo.cat(makeArray(overwrite)),
+        }
+        
+        return outputPromise
+    },
+    stderr: ({ overwrite, appendTo, })=>{
+        if (!(thisTask.stderr instanceof Object)) {
+            thisTask.stderr = {
+                overwrite: [],
+                appendTo: [],
+            }
+        }
+        
+        // merge arguments with any existing ones
+        thisTask.stderr = {
+            overwrite: thisTask.stderr.overwrite.cat(makeArray(overwrite)),
+            appendTo: thisTask.stderr.appendTo.cat(makeArray(overwrite)),
+        }
+        
+        return outputPromise
+    },
+    out: ({ overwrite, appendTo, })=>{
+        if (!(thisTask.out instanceof Object)) {
+            thisTask.out = {
+                overwrite: [],
+                appendTo: [],
+            }
+        }
+        
+        // merge arguments with any existing ones
+        thisTask.out = {
+            overwrite: thisTask.stdout.overwrite.cat(makeArray(overwrite)),
+            appendTo: thisTask.stdout.appendTo.cat(makeArray(overwrite)),
+        }
+        
+        return outputPromise
+    },
+    and: (otherPromise)=>{
+        // stop the other one from executing
+        const otherTask = otherPromise[taskSymbol]
+        otherTask.delay()
+        // hook it into this task
+        thisTask.andTask = otherTask
+        return otherPromise
+    },
+    or: (otherPromise)=>{
+        // stop the other one from executing
+        const otherTask = otherPromise[taskSymbol]
+        otherTask.delay()
+        // hook it into this task
+        thisTask.orTask = otherTask
+        return otherPromise
+    },
+    pipeTo: (otherPromise)=>{
+        // stop the other one from executing
+        const otherTask = otherPromise[taskSymbol]
+        otherTask.delay()
+        thisTask.out = makeArray(thisTask.out)
+        thisTask.out.push(otherTask)
+        return otherPromise
+    },
+    pipeStdoutTo: (otherPromise)=>{
+        // stop the other one from executing
+        const otherTask = otherPromise[taskSymbol]
+        otherTask.delay()
+        thisTask.stdout = makeArray(thisTask.stdout)
+        thisTask.stdout.push(otherTask)
+        return otherPromise
+    },
+    pipeStderrTo: (otherPromise)=>{
+        // stop the other one from executing
+        const otherTask = otherPromise[taskSymbol]
+        otherTask.delay()
+        thisTask.stderr = makeArray(thisTask.stderr)
+        thisTask.stderr.push(otherTask)
+        return otherPromise
+    },
+})
+
+
+// 
+// 
 // the convoluted/messy wrapper
 // 
 // 
-const taskSymbol = Symbol("task")
 function run(...args) {
     const thisTask = {
         command: args,
+        stdin: notGiven,
+        stdout: notGiven,
+        stderr: notGiven,
+        out: notGiven,
         delay: ()=>{
             thisTask.delayed = true
             if (thisTask.cancelId) {
@@ -146,57 +295,7 @@ function run(...args) {
     // 
     // mutate the promise output, make all methods return the promise again (chaining)
     // 
-    Object.assign(outputPromise, {
-        taskSymbol: thisTask,
-        interactive: ({ onOut, onStdout, onStderr, onFinsh, stdin, stdout, out, cwd, env, timeout })=>{
-            Object.assign(thisTask, { stdin, stdout, out, cwd, env, timeout, interactive: {onOut, onStdout, onStderr, onFinsh}, })
-            return outputPromise
-        },
-        with: ({ stdin, stdout, out, cwd, env, timeout })=>{
-            Object.assign(thisTask, { stdin, stdout, out, cwd, env, timeout })
-            return outputPromise
-        },
-        and: (otherPromise)=>{
-            // stop the other one from executing
-            const otherTask = otherPromise[taskSymbol]
-            otherTask.delay()
-            // hook it into this task
-            thisTask.andTask = otherTask
-            return otherPromise
-        },
-        or: (otherPromise)=>{
-            // stop the other one from executing
-            const otherTask = otherPromise[taskSymbol]
-            otherTask.delay()
-            // hook it into this task
-            thisTask.orTask = otherTask
-            return otherPromise
-        },
-        pipeTo: (otherPromise)=>{
-            // stop the other one from executing
-            const otherTask = otherPromise[taskSymbol]
-            otherTask.delay()
-            thisTask.out = makeArray(thisTask.out)
-            thisTask.out.push(otherTask)
-            return otherPromise
-        },
-        pipeStdoutTo: (otherPromise)=>{
-            // stop the other one from executing
-            const otherTask = otherPromise[taskSymbol]
-            otherTask.delay()
-            thisTask.stdout = makeArray(thisTask.stdout)
-            thisTask.stdout.push(otherTask)
-            return otherPromise
-        },
-        pipeStderrTo: (otherPromise)=>{
-            // stop the other one from executing
-            const otherTask = otherPromise[taskSymbol]
-            otherTask.delay()
-            thisTask.stderr = makeArray(thisTask.stderr)
-            thisTask.stderr.push(otherTask)
-            return otherPromise
-        },
-    })
+    Object.assign(outputPromise, methods(thisTask, outputPromise))
 
     return outputPromise
 }
@@ -204,7 +303,7 @@ function run(...args) {
 
 // 
 // 
-// helpers
+// generic helpers
 // 
 // 
 
@@ -217,6 +316,23 @@ const makeArray = (arg) => {
         return [ arg ]
     } else {
         return []
+    }
+}
+
+const assignArgs = (object, args) => {
+    for (const [key, newValue] of Object.entries(args)) {
+        if (newValue !== undefined) {
+            if (newValue instanceof Object) {
+                // merge if both are objects
+                if (object[key] instanceof Object) {
+                    Object.assign(object[key], newValue)
+                } else {
+                    object[key] = newValue
+                }
+            } else {
+                object[key] = newValue
+            }
+        }
     }
 }
 
