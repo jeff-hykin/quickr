31533ab1528c45a885300446e967afaad7261087
Jeff Hykin <jeff.hykin@gmail.com>
2022-08-27 20:29:51 -0500

    fix pathPieces

--- main/file_system.js
+++ main/file_system.js
@@ -611,7 +611,7 @@ export const FileSystem = {
         while (true) {
             folderList.push(Path.basename(dirname))
             // if at the top 
-            if (dirname == result.root) {
+            if (dirname == Path.basename(dirname)) {
                 break
             }
             dirname = Path.dirname(dirname)
