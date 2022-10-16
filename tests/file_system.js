947252e1039e00e163773eb3e9cca3548423ad00
Jeff Hykin <jeff.hykin@gmail.com>
2022-10-16 18:41:05 -0500

    add allParentPaths, shouldntExplore, shouldntReturn

--- tests/file_system.js
+++ tests/file_system.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env -S deno run --allow-all
 const { FileSystem } = await import(`../main/file_system.js`)
-
+import { intersection, subtract } from "https://deno.land/x/good@0.7.6/set.js"
 
 console.debug(`FileSystem.thisFile is:`,FileSystem.thisFile)
 console.debug(`FileSystem.thisFolder is:`,FileSystem.thisFolder)
@@ -10,6 +10,31 @@ await FileSystem.clearAPathFor(`${FileSystem.thisFolder}/run.errors.log`)
 
 console.log(await FileSystem.listFileItemsIn(`../main/`))
 
-console.log(await FileSystem.recursivelyListPathsIn(`..`))
+console.log("# ")
+console.log("# breadthFirstSearch")
+console.log("# ")
+const bfsResults = await FileSystem.recursivelyListPathsIn(`..`, { searchOrder: 'breadthFirstSearch', shouldntReturn: each=>each.match(/.*(^|\/)\.git($|\/).*/) })
+console.log(bfsResults)
 
-console.log(await FileSystem.recursivelyListPathsIn(`..`, { searchOrder: 'depthFirstSearch' }))
+console.log("# ")
+console.log("# depthFirstSearch")
+console.log("# ")
+const dfsResults = await FileSystem.recursivelyListPathsIn(`..`, { searchOrder: 'depthFirstSearch', shouldntExplore: each=>each.match(/.*(^|\/)\.git($|\/).*/) })
+console.log(dfsResults)
+
+const shouldntExplore = subtract({value: bfsResults, from: dfsResults, })
+const shouldntReturn = subtract({value: dfsResults, from: bfsResults, })
+console.debug(`shouldntExplore uniquely has:`,shouldntExplore)
+console.debug(`shouldntReturn uniquely has:`,shouldntReturn)
+
+const anAbsolutePath = `${FileSystem.thisFolder}/run.errors.log`
+console.log("# ")
+console.log(`# All parent paths of ${`${FileSystem.thisFolder}/run.errors.log`}`)
+console.log("# ")
+console.log(FileSystem.allParentPaths(anAbsolutePath))
+
+const aRelativePath = FileSystem.makeRelativePath({ from:".", to:`${FileSystem.thisFolder}/run.errors.log`})
+console.log("# ")
+console.log(`# All parent paths of ${aRelativePath}`)
+console.log("# ")
+console.log(FileSystem.allParentPaths(aRelativePath))
