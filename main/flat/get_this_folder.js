c2372265617e59dd4688e6c87aa069b8f7d0ffce
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-23 08:21:13 -0500

    isolate getThisFolder

---
+++ main/flat/get_this_folder.js
@@ -0,0 +1,25 @@
+import { findAll } from "https://deno.land/x/good@1.6.0.1/string.js"
+const GoodJs = { findAll }
+import { dirname } from "https://deno.land/std@0.128.0/path/mod.ts"
+const PathTools = { dirname }
+import { statSync, cwd } from "https://deno.land/x/deno_deno@1.42.1.7/main.js"
+const Deno = { statSync, cwd }
+
+export function getThisFolder() { // FIXME: fails inside of libraries that are pulled from URL's
+    const err = new Error()
+    const filePaths = GoodJs.findAll(/^.+file:\/\/(\/[\w\W]*?):/gm, err.stack).map(each=>each[1])
+    
+    // if valid file
+    // FIXME: make sure this works inside of anonymous functions (not sure if error stack handles that well)
+    const firstPath = filePaths[0]
+    if (firstPath) {
+        try {
+            if (Deno.statSync(firstPath).isFile) {
+                return PathTools.dirname(firstPath)
+            }
+        } catch (error) {
+        }
+    }
+    // if in an interpreter
+    return Deno.cwd()
+}
