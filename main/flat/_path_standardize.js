9b38c2b9b66a054170254d09f5e62851591745c9
Jeff Hykin <jeff.hykin@gmail.com>
2024-04-23 07:41:57 -0500

    use specific imports

--- main/flat/_path_standardize.js
+++ main/flat/_path_standardize.js
@@ -1,11 +1,11 @@
-import * as Path from "https://deno.land/std@0.128.0/path/mod.ts"
+import { fromFileUrl } from "https://deno.land/std@0.128.0/path/mod.ts"
 
 export const pathStandardize = (path)=>{
     // PathInfo object to path
     path = path.path||path
     // url-like file path to POSIX path
     if (typeof path == 'string' && path.startsWith("file:///")) {
-        path = Path.fromFileUrl(path)
+        path = fromFileUrl(path)
     }
     return path
 }
--- main/flat/make_absolute_path.js
+++ main/flat/make_absolute_path.js
@@ -1,10 +1,10 @@
-import * as Path from "https://deno.land/std@0.128.0/path/mod.ts"
-import Deno from "https://deno.land/x/deno_deno@1.42.1.7/main.js"
+import { normalize, isAbsolute, join } from "https://deno.land/std@0.128.0/path/mod.ts"
+import { cwd } from "https://deno.land/x/deno_deno@1.42.1.7/main.js"
 
 export const makeAbsolutePath = (path)=> {
-    if (!Path.isAbsolute(path)) {
-        return Path.normalize(Path.join(Deno.cwd(), path))
+    if (!isAbsolute(path)) {
+        return normalize(join(cwd(), path))
     } else {
-        return Path.normalize(path)
+        return normalize(path)
     }
 }
