4e3f439d241b2dbdf0940f6a43d98dbd13052187
Jeff Hykin <jeff.hykin@gmail.com>
2025-02-20 15:13:37 -0600

    pathStandardize

---
+++ neo/main/pieces/_pathStandardize.js
@@ -0,0 +1,11 @@
+import { fromFileUrl } from "https://deno.land/std@0.128.0/path/mod.ts"
+
+export const pathStandardize = (path)=>{
+    // PathInfo object to path
+    path = path.path||path
+    // url-like file path to POSIX path
+    if (typeof path == 'string' && path.startsWith("file:///")) {
+        path = fromFileUrl(path)
+    }
+    return path
+}
